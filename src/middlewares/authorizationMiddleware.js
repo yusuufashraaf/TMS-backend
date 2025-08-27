70f008d5d769cce6dc8412dcd5ac61f61497cdcd
Youssef Ashraf <yusuufashraaf@icloud.com>
2025-08-27 20:37:24 +0300

    feat(auth-middleware):add JWT authentication middleware to protect routes

---
+++ src/middlewares/authorizationMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+const protect = (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+  if (!token) {
+    return res.status(401).json({ status: "failed", message: "Not logged in" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ status: "failed", message: "Invalid token" });
+  }
+};
+
+module.exports = protect;
