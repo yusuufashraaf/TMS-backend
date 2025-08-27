6317bfa6998ed3adec6133564ac7e872df5394ba
Youssef Ashraf <yusuufashraaf@icloud.com>
2025-08-27 20:42:34 +0300

    feat(user-route): protect user routes with JWT middleware and restrict delete to admin

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/UserController");
+const authMiddleware = require("../middlewares/authorizationMiddleware");
+
+// Protect all routes
+router.use(authMiddleware.protect);
 
 // CRUD routes
-router.post("/", userController.createUser); // Create user
 router.get("/", userController.getAllUsers); // Get all users
 router.get("/:id", userController.getUserById); // Get single user
 router.put("/:id", userController.updateUser); // Update user
-router.delete("/:id", userController.deleteUser); // Delete user
+
+// Delete user (admin only)
+router.delete(
+  "/:id",
+  authMiddleware.restrictTo("Admin"),
+  userController.deleteUser
+);
 
 module.exports = router;
